8b8def435e0211c20c0524e41340f229c8192fc1
tanis <tanis-y@hotmail.com>
2014-11-20 15:59:15 -0500

    modify crawler.

--- src/edu/nyu/stex/data/crawler/RecentCralwer.java
+++ src/edu/nyu/stex/data/crawler/RecentCralwer.java
@@ -6,15 +6,15 @@ import twitter4j.*;
  * Created by tanis on 10/8/14.
  */
 public class RecentCralwer {
-    public static void main(String args[]) {
-        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
-        TweetsListener listener = new TweetsListener();
-        twitterStream.addListener(listener);
+  public static void main(String args[]) {
+    TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+    TweetsListener listener = new TweetsListener();
+    twitterStream.addListener(listener);
 //        FilterQuery fq = new FilterQuery();
 //        String[] lang = { "en" };
 //        fq.language(lang);
-        twitterStream.sample();
-    }
+    twitterStream.sample();
+  }
 }
 
 
