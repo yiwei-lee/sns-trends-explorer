a55eda184a1d1a23a5d220ac85e7cdf73bdac1aa
Yiwei Li <yl2174@nyu.edu>
2014-12-08 19:32:53 -0500

    Fix mistaken commit.

--- src/edu/nyu/stex/data/source/TwitterSource.java
+++ src/edu/nyu/stex/data/source/TwitterSource.java
@@ -70,13 +70,8 @@ public class TwitterSource extends AbstractSource implements EventDrivenSource,
     StatusListener listener = new StatusListener() {
       public void onStatus(Status status) {
         headers.put("timestamp", String.valueOf(status.getCreatedAt().getTime()));
-<<<<<<< HEAD
-        //Event event = EventBuilder.withBody(TwitterFormatter.toByte(status), headers);
-	Event event = EventBuilder.withBody(TwitterObjectFactory.getRawJSON(status).getBytes(), headers);
-=======
 //        Event event = EventBuilder.withBody(TwitterFormatter.toByte(status), headers);
         Event event = EventBuilder.withBody(TwitterObjectFactory.getRawJSON(status).getBytes(), headers);
->>>>>>> 6cd922ac776f78316e4f2f0ef26cb209b9c3aafd
         channel.processEvent(event);
       }
 
