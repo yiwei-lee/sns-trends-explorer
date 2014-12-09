4d1ec3bd4ef7e7443eceb0acb72f9233bb11f1a4
Yiwei Li <yl2174@nyu.edu>
2014-12-09 18:44:45 -0500

    Add test folder to build source.

--- test/edu/nyu/stex/TestDriver.java
+++ test/edu/nyu/stex/TestDriver.java
@@ -9,6 +9,7 @@ public class TestDriver {
   public static void main(String[] args) throws IOException {
     if (args.length != 1) {
       System.err.println("Usage: Stex.jar cmd [args]");
+      System.exit(1);
     }
     switch (args[0]) {
     case "t2sf":
